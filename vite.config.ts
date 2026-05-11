39ef63616b088611165a5457fa1d762fed3fb16d
qiuyan1209 <815685446@qq.com>
2026-05-11 19:11:03 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig } from '@lark-apaas/fullstack-vite-preset';
 
 export default defineConfig({
+  root: 'client',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'client/src'),
